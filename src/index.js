9b56de95c41a083904fff046b7a306be49589c79
chrislegato <chrislegato@gmail.com>
2025-01-26 13:13:55 -0500

    Reverted to former to fix error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function refreshWeather(response) {
-  const temperatureElement = document.querySelector("#temperature");
-  const temperature = response.data.temperature.current;
-  const cityElement = document.querySelector("#city");
-  const descriptionElement = document.querySelector("#description");
-  const humidityElement = document.querySelector("#humidity");
-  const windSpeedElement = document.querySelector("#wind-speed");
-  const timeElement = document.querySelector("#time");
-  const date = new Date(response.data.time * 1000);
-  const iconElement = document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -21,9 +21,9 @@ function refreshWeather(response) {
 }
 
 function formatDate(date) {
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const days = [
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -32,7 +32,7 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  const day = days[date.getDay()];
+  let day = days[date.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -42,28 +42,28 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  const apiKey = "b2a5adcct04b33178913oc335f405433";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  const searchInput = document.querySelector("#search-form-input");
+  let searchInput = document.querySelector("#search-form-input");
 
   searchCity(searchInput.value.trim);
 }
 
 function formatDay(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[date.getDay()];
 }
 
 function getForecast(city) {
-  const apiKey = "6a23f03a4f5f12tbe00bobfa40e9f907";
-  const apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "6a23f03a4f5f12tbe00bobfa40e9f907";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
@@ -92,11 +92,11 @@ function displayForecast(response) {
     }
   });
 
-  const forecastElement = document.querySelector("#forecast");
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
-const searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Hamilton");
